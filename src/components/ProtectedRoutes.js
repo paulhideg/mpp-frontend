435edf6fbcc9984f7feb83276458eeb5376e66b9
paulhideg <paulhideg002@gmail.com>
2024-05-07 01:55:48 +0300

    changed ProtectedRoutes component redirect

--- src/components/ProtectedRoutes.js
+++ src/components/ProtectedRoutes.js
@@ -6,5 +6,5 @@ const cookies = new Cookies();
 export default function ProtectedRoutes() {
   const token = cookies.get("TOKEN");
   if (!token) alert("Login first to access this resource");
-  return <Fragment>{token ? <Outlet /> : <Navigate to="/home" />}</Fragment>;
+  return <Fragment>{token ? <Outlet /> : <Navigate to="/" />}</Fragment>;
 }
